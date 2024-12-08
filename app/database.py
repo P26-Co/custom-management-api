8cf57af92ccb2c0242135f8a7947bf94ab19bb5a
Abhishek <abhishek@p26.co>
2024-12-08 23:19:13 +0530

    mysql db

--- app/database.py
+++ app/database.py
@@ -2,9 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+from app.config import settings
+
 # Create an instance of SQLite engine
-engine = create_engine("sqlite:///appsavi.db")
-# engine = create_engine("mysql://root:Appsavi0admin1@localhost/appsavi")
+engine = create_engine(settings.DB_URL)
 
 # Create an instance of DeclarativeMeta
 Base = declarative_base()
